773d60455802435ec130bb0b70042e386b46ec08
Yousef Alaa <yousefdev@proton.me>
2026-05-05 23:25:46 +0300

    Fix Bcrypt import issue

--- middleware/verifyPin.js
+++ middleware/verifyPin.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const appError = require("../utils/appError");
 const transactionModel = require("../models/transactionModel");
 
