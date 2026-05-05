b6877e54653094852b14c9e702eab01cf2a3de41
Mahmoud_Nasser <mahmoudnasser80m@gmail.com>
2026-05-05 03:04:31 +0300

    AddRateLimitingForMyroutes

--- middleware/resendLimit.js
+++ middleware/resendLimit.js
@@ -27,14 +27,10 @@ const registerLimiter = rateLimit({
   },
 });
 
-const transactionLimiter = rateLimit({
-  windowMs: 30 * 1000, // 30 seconds
-  max: 1, // allow only 1 transaction every 30 seconds
-  message: {
-    success: false,
-    message:
-      "Transaction in progress. Please wait 30 seconds before trying again.",
-  },
+const verifyEmailLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  message: "Too many OTP attempts, try again later",
 });
 
 const otpLimiter = rateLimit({
@@ -49,10 +45,86 @@ const otpLimiter = rateLimit({
   legacyHeaders: false,
 });
 
+const forgetPasswordLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 3,
+  message: "Too many password reset requests, try again later",
+});
+
+const verifyResetOtpLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  message: "Too many OTP attempts, try again later",
+});
+
+const resetPasswordLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 3,
+  message: "Too many reset attempts, try again later",
+});
+
+const changePasswordLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  message: "Too many password change attempts, try again later",
+});
+
+// User self update
+const updateMeLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 10,
+  message: "Too many profile updates, try again later",
+});
+
+// User delete account
+const deleteMeLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  message: "Too many delete attempts, try again later",
+});
+
+// Admin read users
+const adminGetUsersLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 60,
+});
+
+// Admin update user
+const adminUpdateUserLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 15,
+});
+
+// Admin delete user
+const adminDeleteUserLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+});
+
+const transactionLimiter = rateLimit({
+  windowMs: 30 * 1000, // 30 seconds
+  max: 1, // allow only 1 transaction every 30 seconds
+  message: {
+    success: false,
+    message:
+      "Transaction in progress. Please wait 30 seconds before trying again.",
+  },
+});
+
 module.exports = {
   globalLimiter,
   loginLimiter,
   otpLimiter,
   transactionLimiter,
   registerLimiter,
+  verifyEmailLimiter,
+  forgetPasswordLimiter,
+  verifyResetOtpLimiter,
+  resetPasswordLimiter,
+  changePasswordLimiter,
+  updateMeLimiter,
+  deleteMeLimiter,
+  adminGetUsersLimiter,
+  adminUpdateUserLimiter,
+  adminDeleteUserLimiter,
 };
