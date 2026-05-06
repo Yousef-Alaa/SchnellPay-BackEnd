4f1db598c4e4afa0b44c92a61ddbc6c9cb64a110
Yousef Alaa <yousefdev@proton.me>
2026-05-07 01:43:57 +0300

    Fix Transactions

--- controllers/transactions/transactionsController.js
+++ controllers/transactions/transactionsController.js
@@ -4,7 +4,7 @@ const { sql, poolPromise } = require("../../config/db");
 const AppError = require("../../utils/appError");
 const asyncWrapper = require("../../middleware/asyncWrapper");
 
-const { findByUsername } = require("../../models/userModel");
+const { findByUsername, findById } = require("../../models/userModel");
 const { deductBalance, addBalance } = require("../../models/walletModel");
 const { createTransaction } = require("../../models/transactionModel");
 const { createNotification } = require("../../utils/notificationHelper");
@@ -13,86 +13,81 @@ const { createNotification } = require("../../utils/notificationHelper");
 // @route POST /api/v1/transactions/send
 // @access Private
 exports.sendMoney = asyncWrapper(async (req, res, next) => {
-  const { sender_username, receiver_username, amount, description } = req.body;
+    const { receiver_username, amount, description } = req.body;
 
-  if (!sender_username || !receiver_username || !amount) {
-    return next(AppError.create("Missing required fields", 400, false));
-  }
-  if (sender_username === receiver_username) {
-    return next(AppError.create("Cannot transfer to yourself", 400, false));
-  }
+    if (!receiver_username || !amount) return next(AppError.create("Missing required fields", 400, false));
 
-  if (!amount || amount <= 0) {
-    return next(AppError.create("Invalid amount", 400, false));
-  }
+    if (!amount || amount <= 0) return next(AppError.create("Invalid amount", 400, false));
+    
 
-  const sender = await findByUsername(sender_username);
-  if (!sender) {
-    return next(AppError.create("Sender not found", 400, false));
-  }
+    const [sender, receiver] = await Promise.all([
+        findById(req.user.id),
+        findByUsername(receiver_username)
+    ]);
 
-  const receiver = await findByUsername(receiver_username);
-  if (!receiver) {
-    return next(AppError.create("Receiver not found", 400, false));
-  }
 
-  const pool = await poolPromise;
-  const transaction = new sql.Transaction(pool);
+    if (!sender) return next(AppError.create("Sender not found", 400, false));
+    if (sender.username === receiver_username) return next(AppError.create("Cannot transfer to yourself", 400, false));
+    if (!receiver) return next(AppError.create("Receiver not found", 400, false));
+    
 
-  try {
-    await transaction.begin();
+    const pool = await poolPromise;
+    const transaction = new sql.Transaction(pool);
+    let transactionStarted = false;
+    
+    try {
 
-    const deducted = await deductBalance(transaction, sender.user_id, amount);
+        await transaction.begin();
+        transactionStarted = true;
+        
 
-    if (!deducted) {
-      await transaction.rollback();
-      return next(AppError.create("Insufficient balance", 400, false));
+        const deducted = await deductBalance(transaction, sender.user_id, amount);
+        if (!deducted) throw AppError.create("Insufficient balance", 400, false);
+
+
+        const added = await addBalance(transaction, receiver.user_id, amount);
+        if (!added) throw  AppError.create("Receiver wallet not found", 400, false);
+        
+
+        const refNumber = crypto.randomBytes(8).toString("hex");
+
+        await createTransaction(
+            transaction,
+            sender.user_id,
+            receiver.user_id,
+            amount,
+            description,
+            refNumber,
+        );
+
+        await transaction.commit();
+        transactionStarted = false;
+
+        // Create notifications for both sender and receiver
+        createNotification(
+            sender.user_id,
+            "Money Sent",
+            `You successfully sent ${amount} EGP to ${receiver_username}. Ref: ${refNumber}`,
+            "TRANSACTION",
+            sender.email,
+        );
+
+        createNotification(
+            receiver.user_id,
+            "Money Received",
+            `You received ${amount} EGP from ${sender.username}. Ref: ${refNumber}`,
+            "TRANSACTION",
+            receiver.email,
+        );
+
+        res.json({
+        success: true,
+        message: "Transaction successful",
+        data: { reference: refNumber },
+        });
+
+    } catch (err) {
+        if (transactionStarted) await transaction.rollback();
+        next(err);
     }
-
-    const added = await addBalance(transaction, receiver.user_id, amount);
-
-    if (!added) {
-      await transaction.rollback();
-      return next(AppError.create("Receiver wallet not found", 400, false));
-    }
-
-    const refNumber = crypto.randomBytes(8).toString("hex");
-
-    await createTransaction(
-      transaction,
-      sender.user_id,
-      receiver.user_id,
-      amount,
-      description,
-      refNumber,
-    );
-
-    await transaction.commit();
-
-    // Create notifications for both sender and receiver
-    createNotification(
-      sender.user_id,
-      "Money Sent",
-      `You successfully sent ${amount} EGP to ${receiver_username}. Ref: ${refNumber}`,
-      "TRANSACTION",
-      sender.email,
-    );
-
-    createNotification(
-      receiver.user_id,
-      "Money Received",
-      `You received ${amount} EGP from ${sender_username}. Ref: ${refNumber}`,
-      "TRANSACTION",
-      receiver.email,
-    );
-
-    res.json({
-      success: true,
-      message: "Transaction successful",
-      data: { reference: refNumber },
-    });
-  } catch (err) {
-    await transaction.rollback();
-    next(err);
-  }
 });
